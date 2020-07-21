99b39a0219549f7bfc09d13ebd1045908a7c748c
Sandra Coburn <sandra_coburn@hotmail.com>
2020-07-21 13:42:58 -0700

    Finished binary search recursion

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,7 +1,29 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
-    # Your code here
+    # iterate the array by creating a low pointer and a high pointer
+     #find a base case
+    if end >= start:
+        # start = 0
+        # end = len(arr) -1
 
+        #get a middle index and retrieve the item with that index from list
+        midd_point = (start + end) // 2
+        guess = arr[midd_point]
+       
+        #if element is present at the middle
+        if guess == target:
+            return midd_point
+        #if target is smaller than mid then it can only be present in left subarray
+        if guess > target:
+            return binary_search(arr, target, start, midd_point - 1)
+        else:
+            return binary_search(arr, target, midd_point + 1, end)
+
+    else:
+        #target is ot present in the array
+        return -1
+arr = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7, -4]
+print(binary_search(arr, 9, 0, len(arr)))
 
 # STRETCH: implement an order-agnostic binary search
 # This version of binary search should correctly find 
@@ -11,4 +33,4 @@ def binary_search(arr, target, start, end):
 # or iteratively
 def agnostic_binary_search(arr, target):
     # Your code here
-
+    pass 
